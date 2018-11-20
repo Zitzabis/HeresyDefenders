87921fcaa1d35c6318d7579355b29b6f77b70d11
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-11-19 22:45:54 -0500

    Adds tile textures along with map builder assets.

---
+++ assets/images/HeresyTiles.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="HeresyTiles" tilewidth="1000" tileheight="1000" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="51">
+  <image width="1000" height="1000" source="tiles/pathCorner1.png"/>
+ </tile>
+ <tile id="52">
+  <image width="1000" height="1000" source="tiles/pathCorner2.png"/>
+ </tile>
+ <tile id="53">
+  <image width="1000" height="1000" source="tiles/pathCornerInvert4.png"/>
+ </tile>
+ <tile id="54">
+  <image width="1000" height="1000" source="tiles/pathCornerInvert3.png"/>
+ </tile>
+ <tile id="55">
+  <image width="1000" height="1000" source="tiles/grass.png"/>
+ </tile>
+ <tile id="57">
+  <image width="1000" height="1000" source="tiles/pathCorner4.png"/>
+ </tile>
+ <tile id="58">
+  <image width="1000" height="1000" source="tiles/pathCorner3.png"/>
+ </tile>
+ <tile id="60">
+  <image width="1000" height="1000" source="tiles/pathSideLeft.png"/>
+ </tile>
+ <tile id="61">
+  <image width="1000" height="1000" source="tiles/pathSideRight.png"/>
+ </tile>
+ <tile id="62">
+  <image width="1000" height="1000" source="tiles/rock.png"/>
+ </tile>
+ <tile id="67">
+  <image width="1000" height="1000" source="tiles/pathSide.png"/>
+ </tile>
+ <tile id="68">
+  <image width="1000" height="1000" source="tiles/pathSideDown.png"/>
+ </tile>
+ <tile id="69">
+  <image width="1000" height="1000" source="tiles/pathCornerInvert.png"/>
+ </tile>
+ <tile id="70">
+  <image width="1000" height="1000" source="tiles/pathCornerInvert2.png"/>
+ </tile>
+ <tile id="71">
+  <image width="1000" height="1000" source="tiles/pathCenter.png"/>
+ </tile>
+</tileset>
