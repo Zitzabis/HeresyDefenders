198d4914a76ed85a08b09191501eb9c62e41bf13
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-11-29 22:32:37 -0500

    Moves page component to partial.

--- index.php
+++ index.php
@@ -15,18 +15,8 @@
         }
     </style>
     <body>
-        <div class="container-fluid h-100" style="margin: 0; padding: 0;">
-            <div class="row h-100" style="margin: 0; padding: 0;">
-                <div id="game" class="col h-100" style="margin: 0; padding: 0;">
-                        <script src="js/game.js"></script>
-                </div>
-                <div class="col-4 h-100">
-                    <span id="killPoints">Kill Points: 0</span>
-                    <br>
-                    <button id="spawnWave">Spawn Wave</button>
-                </div>
-            </div>
-        </div>
-        
+        <?php
+            include_once("pages/game.php")  
+        ?>
     </body>
 </html>
---
+++ pages/game.php
@@ -0,0 +1,12 @@
+<div class="container-fluid h-100" style="margin: 0; padding: 0;">
+    <div class="row h-100" style="margin: 0; padding: 0;">
+        <div id="game" class="col h-100" style="margin: 0; padding: 0;">
+                <script src="js/game.js"></script>
+        </div>
+        <div class="col-4 h-100">
+            <span id="killPoints">Kill Points: 0</span>
+            <br>
+            <button id="spawnWave">Spawn Wave</button>
+        </div>
+    </div>
+</div>
