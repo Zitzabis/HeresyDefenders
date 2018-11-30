064631a60bada190f42a0304f4c34981914c36f1
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-11-30 04:03:19 -0500

    Adds credits section.

--- pages/game.php
+++ pages/game.php
@@ -35,6 +35,24 @@
                     <p class="card-text">Use your arrow keys to attack.<br>Only left and right attacks are enabled right now.</p>
                 </div>
             </div>
+            <div class="card">
+                <h5 class="card-header">Credits</h5>
+                <div class="card-body">
+                    <p class="card-text">
+                        <b>Programming/Game Design/Art</b>
+                        <br>
+                        Stephen Floyd
+                        <br><br>
+                        <b>Hero Art</b>
+                        <br>
+                            Rox
+                        <br><br>
+                        <b>Music</b>
+                        <br>
+                        Aaron Brickle
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
